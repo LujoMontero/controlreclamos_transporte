fc611818e706d580ff3de3d94b3631307612eefc
luis <luisjosemon@gmail.com>
2024-08-13 11:07:29 -0400

    realizando pruebas

---
+++ src/main/java/org/example/controlreclamos_transporte/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.example.controlreclamos_transporte.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class HomeController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";  // Devuelve la vista login.html
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";  // Devuelve la vista home.html
+    }
+}
